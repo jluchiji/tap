6841d951771cc101ebfc10c97d3b8c9e4e6e176d
nick-bloomerang <nick.houser@bloomerang.co>
2015-03-03 19:43:53 -0500

    Updated with notes

---
+++ app/src/main/java/com/example/nickhauser/tap/InvokeServer.java
@@ -0,0 +1,17 @@
+package com.example.nickhauser.tap;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Nick Hauser on 3/3/2015.
+ */
+public class InvokeServer extends AsyncTask<String,Integer,JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(String... params) {
+
+        return null;
+    }
+}
